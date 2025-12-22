0572dd553f54a789aed2096b994e9313cb2c45cf
Avro <169134283+Denmerck@users.noreply.github.com>
2025-12-22 14:06:03 -0800

    a

--- module/list.py
+++ module/list.py
@@ -2,4 +2,10 @@
 Set of functions for List
 """
 
-def merge(A: list, B: list) -> list:
+def merge(A: list, B: list, reverse: bool) -> list:
+    """
+    # Merges 2 list toegther \n
+    ## Params
+    :Param A:
+    """
+    

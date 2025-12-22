f5b35296cdd9c5b4713bc9fdd8ddd7340cac68a1
Avro <169134283+Denmerck@users.noreply.github.com>
2025-12-22 14:49:44 -0800

    function finished

--- module/list.py
+++ module/list.py
@@ -5,6 +5,17 @@ Set of functions for List
 def subtract(A: list, B: list):
     """
     # Subtract
-    ## Params
-    :Param A: List to 
+    Removes the matching items\n
+    eg.\n
+        [a, b, c, d, d] - [a, c, e, d] = [a, b, d]
+
+    :param A: Minuend
+    :type A: List
+    :param B: Subtrahend
+    :type B: List
     """
+    for i in A:
+        if i in B:
+            B.remove(i)
+            A.remove(i)
+    return A

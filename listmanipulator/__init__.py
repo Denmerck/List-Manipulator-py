02276d041b263cc4187c0aa2c66dcad8efc550b7
Avro <vincentpangan@icloud.com>
2025-12-25 13:42:23 -0800

    Name changes and __init__.py write

--- listmanipulator/__init__.py
+++ listmanipulator/__init__.py
@@ -5,3 +5,9 @@ The name suggests it, manipulates Lists, Dictionaries and Sets
 """
 
 __version__ = '0.0.1'
+__lisence__ = 'MIT'
+
+__all__ = ['dictutil', 'listutil']
+
+from listmanipulator import listutil
+from listmanipulator import dictutil

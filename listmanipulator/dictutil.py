818eaa64cd0d5fd5d43563ed49fa1de4eeaa0b39
Avro <vincentpangan@icloud.com>
2025-12-26 19:06:06 -0800

    Beginning of dictutil.py

--- listmanipulator/dictutil.py
+++ listmanipulator/dictutil.py
@@ -1,3 +1,12 @@
 """
 Library of dictionary manipulation functions
 """
+
+def reversekeys(A: dict) -> dict:
+    """
+    Reverse the pairs of a dictionary. (Key <-> value)
+    :param A: Target dict
+    :type A: dict
+    :return: Reversed dictionary
+    """
+    pass

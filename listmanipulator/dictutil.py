bf6176359e11904e3ed8403403bc7c54ee6bc29e
Avro <vincentpangan@icloud.com>
2025-12-29 11:01:49 -0800

    New function

--- listmanipulator/dictutil.py
+++ listmanipulator/dictutil.py
@@ -9,4 +9,19 @@ def reversekeys(A: dict) -> dict:
     :type A: dict
     :return: Reversed dictionary
     """
+    cacheA = A.copy()
+    A.clear()
+    for key, value in cacheA.items():
+        A.update({key: value})
+    return A
+
+def merge(A: dict, key1: any, key2: any) -> dict:
+    """
+    Merge two dictionaries. (Key <-> value)
+    :param A: Target dict
+    :param key1: First key
+    :param key2: Second key
+    :return:
+    """
     pass
+    cacheA = A.copy()

3b88e84790262d1b8d48e67cbf67f918717b952d
Avro <vincentpangan@icloud.com>
2026-01-31 08:40:43 -0800

    New function and fixes

--- listmanipulator/dictutil.py
+++ listmanipulator/dictutil.py
@@ -2,62 +2,89 @@
 Library of dictionary manipulation functions
 """
 
-def swap(A: dict) -> dict:
+def swap(Dict: dict) -> dict:
     """
     Swap the pairs of a dictionary. (Key <-> value)
-    :param A: Target dict
-    :type A: dict
+    :param Dict: Target dict
+    :type Dict: dict
     :return:
     """
-    cacheA = A.copy()
-    A.clear()
-    for key, value in cacheA.items():
-        A.update({value: key})
-    return A
+    cacheDict = Dict.copy()
+    Dict.clear()
+    for key, value in cacheDict.items():
+        Dict.update({value: key})
+    return Dict
 
 
-def removevalue(A: dict, value: any) -> dict:
+def removeValue(Dict: dict, value: any) -> dict:
     """
     Remove a value from a dictionary.
-    :param A: Target dict
+    :param Dict: Target dict
     :param value: Value to remove
     :return:
     """
-    for k, v in A.items():
+    for k, v in Dict.items():
         if v == value:
-            A.pop(k)
-    return A
+            Dict.pop(k)
+    return Dict
 
-def values(A: dict)-> list:
+def values(Dict: dict)-> list:
     """
     Get all values in a dictionary.
-    :param A: Target dict
+    :param Dict: Target dict
     :return:
     """
     values = []
-    for k, v in A.items():
+    for k, v in Dict.items():
         values.append(v)
     return values
 
-def keys(A: dict) -> list:
+def keys(Dict: dict) -> list:
     """
     Get all keys in a dictionary.
-    :param A: Target dict
+    :param Dict: Target dict
     :return:
     """
     values = []
-    for k, v in A.items():
+    for k, v in Dict.items():
         values.append(k)
     return values
 
-def findvalue(A: dict, value: list) -> str:
+def findValue(Dict: dict, value: list) -> str:
     """
     Finds a value and returns a key.
-    :param A: Target dict
+    :param Dict: Target dict
     :param value: value to find
-    :return:
     """
-    for k, v in A.items():
+    for k, v in Dict.items():
         if v == value:
             return k
     return None
+
+def replaceKey(Dict: dict, TargetKey: any, Key: any) -> dict:
+    """
+    Replaces a key in a dictionary. returns with no modification if exact key is not found.
+    :param Dict: Target Dict
+    :param TargetKey: Target key
+    :param Key: Replacement key
+    """
+    if TargetKey in Dict:
+        ret = {Key: Dict[TargetKey]}
+        Dict.pop(TargetKey)
+        return Dict + ret
+    else:
+        return Dict
+
+def flatten(Dict: dict) -> dict:
+    """
+    Unpacks a dictionary into its parent dictionary.
+    :param Dict: Target Dict
+    """
+    for k, v in Dict:
+        if type(v) == dict:
+            for k, v in v:
+                Dict.update({k: v})
+            Dict.pop(k)
+            return Dict
+        else:
+            return Dict
